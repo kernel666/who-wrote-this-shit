7886d94fcebb72f8c20dff823914b45a7d32e881
Jim Geovedi <jim.geovedi@gmail.com>
2012-12-05 04:29:35 +0700

    initial import

---
+++ README.md
@@ -0,0 +1,31 @@
+# Who Wrote This Shit
+
+Simple demonstration program to find out who wrote the something online.
+
+
+## Usage
+
+	$ python wwts.py -h
+	usage: wwts.py [-h] -F FILE [-T TAG] [-t] [-g]
+
+	Who wrote this shit?
+
+	optional arguments:
+	  -h, --help            show this help message and exit
+	  -F FILE, --file FILE  Input file
+	  -T TAG, --tag TAG     Tag, e.g. 'Thaddeus T. Grugq' or '@thegruugq'
+	  -t, --train           Training mode
+	  -g, --guess           Guessing mode
+
+
+## Training
+
+	$ python wwts.py --file examples/d99/tweet.txt --train --tag '@d99'
+	$ python wwts.py --file examples/bc/tweet.txt --train --tag '@bennychandra'
+	$ python wwts.py --file examples/gq/tweet.txt --train --tag '@thegrugq'
+
+## Guessing
+
+	$ python wwts.py --file examples/unknown/test.txt --guess
+	[('@bennychandra', 0.8954002478493908), ('@thegrugq', 0.7109256426967558)]
+
---
+++ wwts.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import argparse
+import traceback
+import re
+import codecs
+import lib.bayes as bayes
+
+def main():
+    parser = argparse.ArgumentParser(description='Who wrote this shit?')
+    parser.add_argument('-F', '--file', required=True, help='Input file')
+    parser.add_argument('-T', '--tag', help='Tag, e.g. \'Thaddeus T. Grugq\' or \'@thegruugq\'')
+    parser.add_argument('-t', '--train', action='store_true', help='Training mode')
+    parser.add_argument('-g', '--guess', action='store_true', help='Guessing mode')
+    args = parser.parse_args()
+    
+    gtw = bayes.Bayes()
+    try:
+        gtw.load()
+    except:
+        if args.train:
+            pass
+        elif args.guess:
+            parser.error('cannot load identity file')
+
+    if args.train:
+        if not args.tag:
+            parser.error('argument -T/--tag is required.')
+        with codecs.open(args.file, 'r', encoding='utf-8') as content:
+            gtw.train(args.tag, content.read())
+            gtw.save()
+    elif args.guess:
+        with codecs.open(args.file, 'r', encoding='utf-8') as content:
+            print gtw.guess(content.read())
+    else:
+        parser.error('argument -t/--train or -g/--guess is required')
+
+
+if __name__ == '__main__':
+    try:
+        main()
+        sys.exit(0)
+    except KeyboardInterrupt, e:
+        raise e
+    except SystemExit, e:
+        raise e
+    except Exception, e:
+        print str(e)
+        traceback.print_exc()
+        sys.exit(1)
